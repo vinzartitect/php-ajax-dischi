5c9e172c8b9598636123a970c3d769726e2445ad
VincenzoBorriello89 <vincborr89@gmail.com>
2022-05-09 15:18:53 +0200

    creazione file di base

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+// riporto il file php contenente l array dei dischi
+require_once __DIR__ . '/database/database.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- css link -->
+    <link rel='stylesheet' href='css/style.css'>
+    <title>Spoti-phyp</title>
+</head>
+<body>
+    
+</body>
+</html>
